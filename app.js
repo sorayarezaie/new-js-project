ea6eb94174f0723ca9e9278de4e30ecf4924023b
sorayarezaie <banorezaie2004@gmail.com>
2024-12-31 11:34:33 +0430

    4439

---
+++ app.js
@@ -0,0 +1,24 @@
+const person ={
+    fname:"Soraya";
+    lname:"Rezaie";
+    Age:20;
+    reading : function () {
+        console.log("reading");
+    },
+};
+function person (fname, lname,age){
+    this.fname = fname;
+    this.lnmae = lname;
+    this.age= age;
+}
+person.prototype.reading = function (){}
+    cont person1 = new person ("Soraya", "Rezaie", 20);
+    const person2 = new person ("Sakina", "Rezaie",18);
+    console.log(person1.fname);
+    console.log(person2.lname);
+
+
+let {fname, lname, age, gender}= person;
+
+console.log(fname);
+console.log(lname);

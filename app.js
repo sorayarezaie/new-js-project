4c02bf6de757311861746725cabdbae68d637e07
sorayarezaie <banorezaie2004@gmail.com>
2024-12-31 12:09:33 +0430

    433

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ function person (fname, lname,age){
     this.fname = fname;
     this.lnmae = lname;
     this.age= age;
+    this.gender = female ;
 }
 person.prototype.reading = function (){}
-    const person1 = new person ("Soraya", "Rezaie", 20);
-    const person2 = new person ("Sakina", "Rezaie",18);
+    const person1 = new person ("Soraya", "Rezaie", 20, female);
+    const person2 = new person ("Sakina", "Rezaie",18, female);
     console.log(person1.fname);
     console.log(person2.lname);
 

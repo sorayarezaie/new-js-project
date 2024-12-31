c0b71483100c43ce32c720b8a1bc8a4dbd736f1a
sorayarezaie <banorezaie2004@gmail.com>
2024-12-31 18:57:24 +0430

    function added

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const person ={
-    fname:"Soraya",
-    lname:"Rezaie",
-    Age:20,
-    gender : "female",
-    reading : function () {
-        console.log("reading");
-    },
-};
-function person (fname, lname,age){
-    this.fname = fname;
-    this.lnmae = lname;
-    this.age= age;
-    this.gender = female ;
+// const person ={
+//     fname:"Soraya",
+//     lname:"Rezaie",
+//     Age:20,
+//     gender : "female",
+//     reading : function () {
+//         console.log("reading");
+//     },
+// };
+// function person (fname, lname,age){
+//     this.fname = fname;
+//     this.lnmae = lname;
+//     this.age= age;
+//     this.gender = female ;
+// }
+// person.prototype.reading = function (){}
+//     const person1 = new person ("Soraya", "Rezaie", 20, female);
+//     const person2 = new person ("Sakina", "Rezaie",18, female);
+//     console.log(person1.fname);
+//     console.log(person2.lname);
+
+
+// let {fname, lname, age, gender}= person;
+
+// console.log(fname);
+// console.log(lname);
+
+function greeting(){
+    console.log("hello");
 }
-person.prototype.reading = function (){}
-    const person1 = new person ("Soraya", "Rezaie", 20, female);
-    const person2 = new person ("Sakina", "Rezaie",18, female);
-    console.log(person1.fname);
-    console.log(person2.lname);
-
-
-let {fname, lname, age, gender}= person;
-
-console.log(fname);
-console.log(lname);
+console.log(typeof greeting);

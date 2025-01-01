85a0dd8804dc0aa31e4d778a1d9f7997ac8086e6
sorayarezaie <banorezaie2004@gmail.com>
2025-01-01 14:52:08 +0430

    89038

--- app.js
+++ app.js
@@ -58,13 +58,26 @@
 //     console.log("odd");
 // }
 
-let x =0;
-if (x == 0){
+// let x =0;
+// if (x == 0){
+//     console.log("zero");
+// }
+// else if (x > 0){
+//     console.log("positive");
+// }
+// else{
+//     console.log("negative");
+// }
+
+
+ let x = 13;
+ if (x == 0 ){
     console.log("zero");
-}
-else if (x > 0){
-    console.log("positive");
-}
-else{
-    console.log("negative");
+ }else{
+    if (x % 2 == 0 ){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
 }

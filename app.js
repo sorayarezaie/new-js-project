3ec274f0ad941adf0d1a8e8126efb42bad8a7ebb
sorayarezaie <banorezaie2004@gmail.com>
2025-01-01 14:40:05 +0430

    p and n added

--- app.js
+++ app.js
@@ -47,13 +47,24 @@
 //     console.log("hello world");
 // }
 
-let x = 0;
-if (x==0){
+// let x = 0;
+// if (x==0){
+//     console.log("zero");
+// }
+// else if (x%2 ==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+let x =0;
+if (x == 0){
     console.log("zero");
 }
-else if (x%2 ==0){
-    console.log("even");
+else if (x > 0){
+    console.log("positive");
 }
 else{
-    console.log("odd");
+    console.log("negative");
 }
